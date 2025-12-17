63289891dd1fe253704c8bd82d2c34a3dd58a48e
amatraci <149633690+amatraci@users.noreply.github.com>
2025-12-17 22:45:29 +0100

    moving code to camera page

---
+++ intoKosova/app/camera.tsx
@@ -0,0 +1,47 @@
+import { View, Pressable } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRef, useEffect } from "react";
+
+export default function CameraScreen() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<CameraView | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!permission?.granted) {
+      requestPermission();
+    }
+  }, []);
+
+  if (!permission?.granted) {
+    return null;
+  }
+
+  const takePhoto = async () => {
+    if (!cameraRef.current) return;
+
+    const photo = await cameraRef.current.takePictureAsync({ quality: 0.8 });
+    router.back(); // ose router.replace me param
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+      <CameraView ref={cameraRef} style={{ flex: 1 }} facing="back" />
+
+      <Pressable
+        onPress={takePhoto}
+        style={{
+          position: "absolute",
+          bottom: 40,
+          alignSelf: "center",
+          width: 70,
+          height: 70,
+          borderRadius: 35,
+          backgroundColor: "#fff",
+        }}
+      />
+    </SafeAreaView>
+  );
+}
